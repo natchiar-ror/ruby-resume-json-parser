abb87b21dadfc59647556c9c553340a9a8d272a7
natchiar <natchiar.v@gmail.com>
2018-12-01 13:29:55 +0530

    Adding view the parsed json

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
 	  
+	  @file = File.read(Rails.root+'public/resume.json')
+	  @result = JSON.parse(@file)
+	  puts @result.inspect
+	  
   end
 end
